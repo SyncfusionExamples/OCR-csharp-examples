8cea914d221f7ca294ea291466fc8b8f0ef6412b
sameerkhan001 <sameerkhan.n@syncfusion.com>
2022-12-20 17:11:26 +0530

    EJDOTNETCORE-4307 Added OCR readme file and sample in docker and Linux

---
+++ Docker/Perform_OCR_Docker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Perform_OCR_Docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Linux/Perform_OCR_Linux/Program.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.OCRProcessor;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+
+string path = ("Tesseractbinaries/Linux/");
+string docPath = ("input.pdf");
+
+//Initialize the OCR processor
+using (OCRProcessor processor = new OCRProcessor(path))
+{
+    //Load the PDF document 
+    FileStream stream = new FileStream(docPath, FileMode.Open, FileAccess.Read);
+    PdfLoadedDocument lDoc = new PdfLoadedDocument(stream);
+
+    //Language to process the OCR
+    processor.Settings.Language = Languages.English;
+    //Process OCR by providing loaded PDF document, Data dictionary and language
+    processor.PerformOCR(lDoc, ("tessdata/"));
+
+    //Save the OCR processed PDF document in the disk
+    MemoryStream streamData = new MemoryStream();
+    lDoc.Save(streamData);
+    File.WriteAllBytes("Output.pdf", streamData.ToArray());
+    lDoc.Close(true);
+
+}
