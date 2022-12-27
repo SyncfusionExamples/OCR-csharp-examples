cd4245662b5a00e9627031b8eae20d0d71771253
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2022-12-27 12:12:29 +0530

    updated changes for core project

--- Perform_OCR_NET_Core/Perform_OCR_NET_Core/Controllers/HomeController.cs
+++ Perform_OCR_NET_Core/Perform_OCR_NET_Core/Controllers/HomeController.cs
@@ -1,42 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Perform_OCR_NET_Core.Models;
-using System;
 using Syncfusion.OCRProcessor;
 using Syncfusion.Pdf.Parsing;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
 
 namespace Perform_OCR_NET_Core.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IHostingEnvironment _hostingEnvironment;
-
-
         public HomeController(IHostingEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
         public IActionResult PerformOCR()
         {
@@ -48,7 +30,7 @@ namespace Perform_OCR_NET_Core.Controllers
                 FileStream fileStream = new FileStream(docPath, FileMode.Open, FileAccess.Read);
                 //Load a PDF document
                 PdfLoadedDocument lDoc = new PdfLoadedDocument(fileStream);
-                //Set OCR language to process
+                //Set OCR language to process.
                 processor.Settings.Language = Languages.English;
                 string tessdataPath = _hostingEnvironment.WebRootPath + "/Data/tessdata";
                 //Process OCR by providing the PDF document and Tesseract data
@@ -66,6 +48,21 @@ namespace Perform_OCR_NET_Core.Controllers
                 return fileStreamResult;
             }
 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
